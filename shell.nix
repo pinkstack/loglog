23cc8457b68d8dae2c12af700aedd39073204691
Oto Brglez <otobrglez@gmail.com>
2022-11-07 13:14:52 +0100

    Removing node junk.

--- shell.nix
+++ shell.nix
@@ -5,18 +5,13 @@ mkShell {
     ansible
     influxdb
     jdk17_headless
-    redis
     ruby
     sbt
-    nodejs
-    yarn
     flyctl
   ];
   shellHook = ''
     export LOGLOG_HOME=`pwd`
     export ANSIBLE_HOST_KEY_CHECKING=False
     export ANSIBLE_INVENTORY=inventory.yaml
-    export PATH="$PWD/node_modules/.bin/:$PATH"
-    yarn install
   '';
 }
